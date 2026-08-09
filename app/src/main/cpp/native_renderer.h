0200608e72e224ca9bacf5183100fadb14e212aa
Saber <112959097+Saber153@users.noreply.github.com>
2026-08-09 21:31:09 +0800

    Create native_renderer.h

---
+++ app/src/main/cpp/native_renderer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <android/native_window.h>
+#include <jni.h>
+
+struct EglState {
+    void* display;
+    void* surface;
+    void* context;
+};
+
+EglState egl_init(ANativeWindow* window);
+void render_texture(unsigned int tex_id, int width, int height);
+void egl_destroy(EglState& state);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT long JNICALL
+Java_com_example_usbcap_EglHelper_nativeInit(JNIEnv* env, jobject thiz, jobject surface);
+
+JNIEXPORT void JNICALL
+Java_com_example_usbcap_EglHelper_nativeRender(JNIEnv* env, jobject thiz, long handle, jint texId, jint width, jint height);
+
+JNIEXPORT void JNICALL
+Java_com_example_usbcap_EglHelper_nativeRelease(JNIEnv* env, jobject thiz, long handle);
+
+#ifdef __cplusplus
+}
+#endif
